d66950b589e0c2fce04b4f2ab3714ba35705b710
Joe Reinhart <joseph.reinhart@gmail.com>
2026-02-04 12:43:33 -0800

    Add missing import for key composable

--- app/src/main/java/com/capyreader/app/ui/articles/ArticleScreen.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/ArticleScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
